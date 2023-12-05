69e48bc857358e26dc05ff7296bdd3de1733be64
Rahma Abdelkader <93728513+rahmaabdelkader2@users.noreply.github.com>
2023-12-05 19:50:15 +0200

    Update receiver.ino

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,6 +1,7 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
+unsigned long startTime;
 
 // Structure example to receive data
 // Must match the sender structure
@@ -22,6 +23,24 @@ float angle_p;
 // Create a struct_message called myData
 struct_message myData;
 
+void setup() {
+  // Initialize Serial Monitor
+  Serial.begin(115200);
+  startTime = millis();
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_register_recv_cb(OnDataRecv);
+}
+ 
+
 // callback function that will be executed when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len)
 {
@@ -31,21 +50,31 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len)
   {
     roll_1 = myData.Roll;
     pitch_1 = myData.Pitch;
+    double elapsedTime_1 =( (millis() - startTime) / 1000.0);
 
     Serial.print("Thigh roll: ");
     Serial.println(roll_1);
     Serial.print("thigh pitch");
     Serial.println(pitch_1);
+
+    Serial.print("\tTime (s): ");
+    Serial.println(elapsedTime_1);
+    
   }
   else
   {
     roll_2 = myData.Roll;
     pitch_2 = myData.Pitch;
+    double elapsedTime_2 =( (millis() - startTime) / 1000.0);
 
     Serial.print("Calf roll: ");
     Serial.println(roll_2);
     Serial.print("Calf pitch");
     Serial.println(pitch_2);
+
+    Serial.print("\tTime (s): ");
+    Serial.println(elapsedTime_2);
+
   }
   // delay(1000);
   // Serial.print("ESP No: ");
@@ -61,23 +90,7 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len)
   delay(1000);
 }
  
-void setup() {
-  // Initialize Serial Monitor
-  Serial.begin(115200);
-  
-  // Set device as a Wi-Fi Station
-  WiFi.mode(WIFI_STA);
-  // Init ESP-NOW
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initializing ESP-NOW");
-    return;
-  }
-  
-  // Once ESPNow is successfully Init, we will register for recv CB to
-  // get recv packer info
-  esp_now_register_recv_cb(OnDataRecv);
-}
- 
+
 void loop() {
 
 }
