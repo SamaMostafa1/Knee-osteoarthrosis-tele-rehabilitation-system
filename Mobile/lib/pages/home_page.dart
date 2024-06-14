cad955da164cdf0d0f3b881d8e2986172f4c284b
jooo71 <youseifessam70@gmail.com>
2024-06-14 15:50:22 +0300

    upload profile image

--- Mobile/lib/pages/home_page.dart
+++ Mobile/lib/pages/home_page.dart
@@ -1,10 +1,16 @@
+import 'dart:typed_data';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:modern_login/pages/login_page.dart';
+import 'package:modern_login/utils.dart';
 import '../Models/user_info.dart';
 import 'Splash.dart';
+import '../utils.dart';
+import '../resources/add_data.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -16,7 +22,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final user = FirebaseAuth.instance.currentUser!;
   final DatabaseReference dbRef =
-  FirebaseDatabase.instance.reference().child("data");
+      FirebaseDatabase.instance.reference().child("data");
 
   TextEditingController _textFieldController1 = TextEditingController();
   TextEditingController _textFieldController2 = TextEditingController();
@@ -24,8 +30,9 @@ class _HomePageState extends State<HomePage> {
   TextEditingController _textFieldController4 = TextEditingController();
 
   List<String> fieldTitles = ['Name', 'Age', 'Weight', 'Length'];
-  var flag=0;
-
+  var flag = 0;
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+  String? _imageUrl;
   bool showFields = false;
   bool showSave = false;
   void toggleFieldsVisibility() {
@@ -55,11 +62,16 @@ class _HomePageState extends State<HomePage> {
     }).catchError((error) {
       print("Error reading data: $error");
     });
+    fetchUserImage();
   }
 
   void signUserOut() {
     FirebaseAuth.instance.signOut();
-    Navigator.push(context, MaterialPageRoute(builder: (context) => Splash(),));
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => Splash(),
+        ));
   }
 
   void writeData() {
@@ -82,6 +94,32 @@ class _HomePageState extends State<HomePage> {
       print("Error writing data: $error");
     });
   }
+  Uint8List ? _image;
+  void selectImage() async{
+    Uint8List img = await  pickImage(ImageSource.gallery);
+    setState(() {
+      _image = img;
+    });
+    if(img!=null){
+      saveProfile();
+    }
+  }
+
+  void saveProfile() async{
+    // String name =dbRef.child(user.uid).get().then((DataSnapshot data);
+    String resp = await StoreData().saveData(name: user.uid, file: _image!);
+
+  }
+  Future<void> fetchUserImage() async {
+    try {
+      String downloadURL = await _storage.ref(user.uid).getDownloadURL();
+      setState(() {
+        _imageUrl = downloadURL;
+      });
+    } catch (error) {
+      print("Error fetching image: $error");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -97,73 +135,116 @@ class _HomePageState extends State<HomePage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
+            Stack(
+              children: [
+                _image != null
+                    ? CircleAvatar(
+                  radius: 64,
+                  backgroundImage: MemoryImage(_image!),
+                )
+                    : _imageUrl != null
+                    ? CircleAvatar(
+                  radius: 64,
+                  backgroundImage: NetworkImage(_imageUrl!),
+                )
+                    : const CircleAvatar(
+                  radius: 64,
+                  backgroundImage: NetworkImage(
+                    "https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=BIbFwuv7FxTWvh5S3vB6bkT0Qv8Vn8N5Ffseq84ClGI=",
+                  ),
+                ),
+                Positioned(
+                    child: IconButton(
+                  onPressed: selectImage,
+                  icon: (const Icon(Icons.add_a_photo)),
+                ),
+                  bottom: -10,
+                  left: 80,
+                )
+              ],
+            ),
             Center(
               child: Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: Text("Logged IN as: ${user.email}"),
               ),
             ),
-
-            if(showFields)
+            if (showFields)
               for (int i = 0; i < fieldTitles.length; i++)
                 Padding(
                   padding: const EdgeInsets.all(16.0),
-                  child: TextField(style: TextStyle(color: Colors.white),
-                    keyboardType: i != 0 ? TextInputType.number : TextInputType.text,
+                  child: TextField(
+                    style: TextStyle(color: Colors.white),
+                    keyboardType:
+                        i != 0 ? TextInputType.number : TextInputType.text,
                     controller: i == 0
                         ? _textFieldController1
                         : i == 1
-                        ? _textFieldController2
+                            ? _textFieldController2
+                            : i == 2
+                                ? _textFieldController3
+                                : _textFieldController4,
+                    maxLength: i == 0
+                        ? 21
+                        : i == 1
+                            ? 3
+                            : i == 2
+                                ? 3
+                                : 3,
+                    onChanged: i == 1
+                        ? (value) {
+                            flag = 0;
+                            if (int.tryParse(value) != null) {
+                              int lengthValue = int.parse(value);
+                              if (lengthValue > 130) {
+                                flag = 1;
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                    content:
+                                        Text('Maximum value allowed is 130'),
+                                  ),
+                                ); // Set the value to 300 if it's greater
+                              }
+                            }
+                          }
                         : i == 2
-                        ? _textFieldController3
-                        : _textFieldController4,
-                    maxLength: i ==0?21 : i==1?3 : i==2?3 : 3,
-                    onChanged: i ==1?(value) {
-                      flag=0;
-                      if (int.tryParse(value) != null) {
-                        int lengthValue = int.parse(value);
-                        if (lengthValue > 130) {
-                          flag =1;
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content: Text('Maximum value allowed is 130'),
-                            ),
-                          ); // Set the value to 300 if it's greater
-                        }
-                      }
-                    }
-                    :i==2?(value) {
-                      flag=0;
-                      if (int.tryParse(value) != null) {
-                        int lengthValue = int.parse(value);
-                        if (lengthValue > 500) {
-                          flag =1;
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content: Text('Maximum value allowed is 500'),
-                            ),
-                          ); // Set the value to 300 if it's greater
-                        }
-                      }
-                    }
-                    :(value) {
-                      flag=0;
-                      if (int.tryParse(value) != null) {
-                        int lengthValue = int.parse(value);
-                        if (lengthValue > 300) {
-                          flag =1;
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content: Text('Maximum value allowed is 300'),
-                            ),
-                          ); // Set the value to 300 if it's greater
-                        }
-                      }
-                    },
+                            ? (value) {
+                                flag = 0;
+                                if (int.tryParse(value) != null) {
+                                  int lengthValue = int.parse(value);
+                                  if (lengthValue > 500) {
+                                    flag = 1;
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                        content: Text(
+                                            'Maximum value allowed is 500'),
+                                      ),
+                                    ); // Set the value to 300 if it's greater
+                                  }
+                                }
+                              }
+                            : (value) {
+                                flag = 0;
+                                if (int.tryParse(value) != null) {
+                                  int lengthValue = int.parse(value);
+                                  if (lengthValue > 300) {
+                                    flag = 1;
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                        content: Text(
+                                            'Maximum value allowed is 300'),
+                                      ),
+                                    ); // Set the value to 300 if it's greater
+                                  }
+                                }
+                              },
                     decoration: InputDecoration(
-                      hintText: 'Enter ${fieldTitles[i]}',
-                      labelText: fieldTitles[i],labelStyle: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 20)
-                    ),
+                        hintText: 'Enter ${fieldTitles[i]}',
+                        labelText: fieldTitles[i],
+                        labelStyle: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20)),
                   ),
                 ),
             Row(
@@ -172,6 +253,7 @@ class _HomePageState extends State<HomePage> {
                   Expanded(
                     child: ElevatedButton(
                       onPressed: () {
+
                         flag == 0 ? writeData() : 1;
                       },
                       child: Text('Save'),
@@ -187,11 +269,9 @@ class _HomePageState extends State<HomePage> {
               ],
             ),
           ],
-
         ),
       ),
       backgroundColor: Colors.blueAccent,
     );
   }
 }
-
