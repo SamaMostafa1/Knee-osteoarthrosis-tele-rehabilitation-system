86512ca0e25ffcfdb98370e8416874ebb0dc2ad3
jooo71 <youseifessam70@gmail.com>
2024-07-07 16:09:02 +0300

    instruction page

--- Mobile/lib/pages/instructions_screen/instruction_page1.dart
+++ Mobile/lib/pages/instructions_screen/instruction_page1.dart
@@ -5,10 +5,41 @@ class instructionPage1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // color: Colors.red,
-      child: Center(
-        child: Text("Page1"),
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Linear gradient background
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.blue, Colors.white54], // Replace with your desired colors
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.2, // Adjust the value as needed
+            left: 0,
+            right: 0,
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Icon(
+                Icons.battery_charging_full_outlined, // Use your desired icon
+                size: 100, // Adjust the size as needed
+                color: Colors.blue.shade900,
+              ),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            child: Text(
+              'Battery working around 45 minute.',
+              style: TextStyle(fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
       ),
     );
   }
--- Mobile/lib/pages/instructions_screen/instruction_page3.dart
+++ Mobile/lib/pages/instructions_screen/instruction_page3.dart
@@ -5,10 +5,46 @@ class instructionPage3 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
-      child: Center(
-        child: Text("Page3"),
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Linear gradient background
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.blue, Colors.white54], // Replace with your desired colors
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.2, // Adjust the value as needed
+            left: 0,
+            right: 0,
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Icon(
+                Icons.chair_alt, // Use your desired icon
+                size: 100, // Adjust the size as needed
+                color: Colors.black,
+              ),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.symmetric(horizontal: 16.0), // Add padding to control line breaks
+            child: RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                style: TextStyle(fontSize: 20, color: Colors.black),
+                children: [
+                  TextSpan(text: 'its prefer to use chair without any arms. '),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
